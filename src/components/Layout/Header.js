6e58cabb5eae12a20d482174693d02d3e4044939
esuom1 <yerdna.voleyrog@gmail.com>
2019-05-08 23:29:58 -0700

    Working template

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+
+import { Link } from 'react-router-dom';
+import * as ROUTES from '../../constants/routes';
+
+import Firebase from '../firebase'
+
+function HeaderAppBar(props) {
+
+  function logout() {
+    const firebase = new Firebase();
+    firebase.signOut();
+  }
+
+  let button;
+  if (props.authUser) {
+    button = <Button component={Link} to={ROUTES.LANDING} onClick={logout} color="inherit">Logout</Button>;
+  } else {
+    button = <Button component={Link} to={ROUTES.SIGN_IN} color="inherit">Login</Button>;
+  }
+
+  return (
+    <div className="header-root">
+      <AppBar style={{background: '#18EAAF', position: 'fixed', top: 0}} position="static">
+        <Toolbar>
+          <IconButton className="menuButton" color="inherit">
+            <MenuIcon />
+          </IconButton>
+          <Typography style={{ flexGrow: '1'}} variant="h6" color="inherit" className="grow">
+            PICKR
+          </Typography>
+          {button}
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
+
+export default HeaderAppBar;
---
+++ src/components/Pages/Favourites.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Favourites = () => (
+  <div>
+    <h1>Favourites Page</h1>
+  </div>
+);
+
+export default Favourites;
