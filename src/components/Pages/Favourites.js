7e64ad7a2dae39603eacc96717c3614fb2cb949a
Steven Hwang <hwang.steven.90@gmail.com>
2019-05-16 14:23:45 -0700

    push sprint 5 progress

--- src/components/Pages/Favourites.js
+++ src/components/Pages/Favourites.js
@@ -23,6 +23,9 @@ class Favourites extends React.Component {
             // List of quests favourited by the user
             list: null,
 
+            // List of quests completed by the user
+            completed: null,
+
             // Boolean set to true until user is logged in and list updated
             loading: true,
 
@@ -31,6 +34,12 @@ class Favourites extends React.Component {
 
             // an array of quests favourited by the user
             favouriteArray: [],
+
+            // an array of quests completed by the user
+            completedArray: [],
+
+            // an array of user progress xp by categories
+            progressArray: [],
         };
     }
 
@@ -55,7 +64,15 @@ class Favourites extends React.Component {
               list: snapshot.val(),
               loading: false,
             });
-            this.updateArray();
+            this.updateFavouriteArray();
+          });
+
+          this.firebase.completed(user.uid).once("value", snapshot => {
+            console.log(snapshot.val());
+            this.setState({ 
+              completed: snapshot.val(),
+            });
+            this.updateCompletedArray();
           });
         } else {
           //not logged in
@@ -65,7 +82,7 @@ class Favourites extends React.Component {
     }
 
     // updates user Favourite Quests array
-    updateArray() {
+    updateFavouriteArray() {
       let array= [];
       for (var quest in this.state.list) {
         console.log(quest);
@@ -83,6 +100,24 @@ class Favourites extends React.Component {
       });
     }
 
+    updateCompletedArray() {
+      let array= [];
+      for (var quest in this.state.completed) {
+        console.log(quest);
+        let questID = this.state.completed[quest].questID;
+        let completedDate = this.state.completed[quest].completedDate;
+        let currentQuest = {
+          questID: questID,
+          completedDate: completedDate,
+        };
+        console.log("adding quest");
+        array.push(currentQuest);
+      }
+      this.setState({
+        completedArray: array,
+      });
+    }
+
     // deletes the quest associated with the delete button that called this
     deleteQuest(event) {
             console.log("quest to be deleted " + event.currentTarget.value);
@@ -94,10 +129,76 @@ class Favourites extends React.Component {
                 list: snapshot.val(),  
               });
 
-              this.updateArray();
+              this.updateFavouriteArray();
 
     });
   }
+
+  // completes the quest associated with the completed button that called this
+  completeQuest(event) {
+    let now = new Date().toString(' MMMM d yyyy');;
+    this.firebase.completed(this.globalUser.uid).child(event.currentTarget.value).update({
+      questID : event.currentTarget.value,
+      completedDate : now
+    });
+
+    // this.firebase.favourites(this.globalUser.uid).child(event.currentTarget.value).remove();
+
+    // this.firebase.favourites(this.globalUser.uid).once("value", snapshot => {
+    //   console.log(snapshot.val());
+    //   this.setState({ 
+    //     list: snapshot.val(),  
+    //   });
+
+    //   this.updateFavouriteArray();
+
+    // });
+
+    this.firebase.completed(this.globalUser.uid).once("value", snapshot => {
+    this.setState({ 
+      completed: snapshot.val(),  
+    });
+
+    this.firebase.categoryProgress(this.globalUser.uid).once("value", snapshot => {
+      this.updateProgressArray(snapshot.val());
+    })
+
+
+    this.updateCompletedArray();
+
+  });
+}
+
+// update progressArray before adding xp of quest to be completed
+updateProgressArray(snap) {
+  let array = [];
+  for (var category in snap) {
+    let currentCategory =category;
+    let currentXP = snap[category];
+    let currentProgress = {
+      category: currentCategory,
+      xp: currentXP,
+    };
+    array.push(currentProgress);
+  };
+}
+
+
+      // deletes the quest associated with the delete button that called this
+      clearQuest(event) {
+        console.log("quest to be cleared " + event.currentTarget.value);
+        this.firebase.completed(this.globalUser.uid).child(event.currentTarget.value).remove();
+
+        this.firebase.completed(this.globalUser.uid).once("value", snapshot => {
+          console.log(snapshot.val());
+          this.setState({ 
+            completed: snapshot.val(),  
+          });
+
+          this.updateCompletedArray();
+
+});
+}
 
     createList = () => {
       // Array of favourited quests in JSX syntax
@@ -145,6 +246,9 @@ class Favourites extends React.Component {
                 <button className="delete" value = {this.state.favouriteArray[i].questID} onClick={this.deleteQuest.bind(this)
                   }>Delete
                 </button>
+                <button className="completed" value = {this.state.favouriteArray[i].questID} onClick={this.completeQuest.bind(this)
+                  }>Completed!
+                </button>
                 <img src="../../red_x.png" />
         
               </div>
@@ -160,14 +264,75 @@ class Favourites extends React.Component {
       
     }
 
+    createCompletedList = () => {
+      // Array of completed quests in JSX syntax
+      let completedList = []
+
+      // if completed quests list is empty render a blank list
+      if(this.state.completedArray.length === 0) {
+        console.log("no favourites");
+        return <div>No Quests Completed</div>
+      }
+
+      // if completed quests list is not empty
+
+      // iterate through every quest in completedArray and create a JSX list for the quest
+      for (let i = 0; i < this.state.completedArray.length; i++) {
+        console.log(i +"completed length " + this.state.completedArray.length);
+        if (this.state.completedArray[i] != undefined) {
+
+          let questName = JSON.stringify(this.state.questList[this.state.completedArray[i].questID].name);
+          questName = questName.substring(1, questName.length -1);
+
+          let questDescription = JSON.stringify(this.state.questList[this.state.completedArray[i].questID].description);
+          questDescription = questDescription.substring(1, questDescription.length -1);
+
+          // let questImageLink = JSON.stringify(this.state.questList[this.state.list[i].questID].imgLink);
+          // questImageLink = questImageLink.substring(1, questImageLink.length -1);
+
+          console.log(this.state.completedArray);
+          let completedDate = JSON.stringify(this.state.completedArray[i].completedDate);
+          completedDate = completedDate.substring(1, completedDate.length -1);
+
+          completedList.push(
+            <div className = "completedContainer">
+              <div className = "completedTop">
+                <div className = "questName">{questName}</div>
+                <div className = "completedDate">
+                  Completed On: {completedDate}
+                </div>
+              </div>
+              <div className = "completedBottom">
+              <div className = "questDescription">
+                {questDescription}
+              </div>
+              <div className = "completedButtons">
+                <button className="clear" value = {this.state.completedArray[i].questID} onClick={this.clearQuest.bind(this)
+                  }>Clear
+                </button>
+                <img src="../../red_x.png" />
+        
+              </div>
+
+              </div>
+ 
+            </div>
+          
+          )
+        }
+      }
+      return completedList;
+
+      
+    }
+
 
     render() {
         let content;
         if (!this.state.loading) {
 
           console.log("rendering");
-          content = <div> {this.createList()} </div>;
-          
+          content = <div><div className = "favourites">Favourited Quests {this.createList()} </div> <div className = "completed">Completed Quests {this.createCompletedList()}</div></div>
         } else {
           content = 
           <div>
