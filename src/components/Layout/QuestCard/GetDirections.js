d43d2d0422352f1185f41960c8a9923692d02830
NoahMacRitchie <noah21mac@gmail.com>
2019-05-22 13:33:48 -0700

    Updated Documentation

--- src/components/Layout/QuestCard/GetDirections.js
+++ src/components/Layout/QuestCard/GetDirections.js
@@ -1,27 +1,32 @@
 import React from "react";
-import Grid from '@material-ui/core/Grid';
+// Material UI's utility and styling components
 import Button from '@material-ui/core/Button';
 import LinearProgress from '@material-ui/core/LinearProgress';
 import WalkIcon from '@material-ui/icons/DirectionsWalk'
 import BikeIcon from '@material-ui/icons/DirectionsBike';
 import TransitIcon from '@material-ui/icons/DirectionsTransit';
 import { Divider } from "@material-ui/core";
+
+/* The GetDirections class makes distance matrix requests to google to get journey times.
+    Once you click the get travel times button it displays the time to walk, bike and transit
+     to get to the location. */
 class GetDirections extends React.Component {
     constructor(props) {
         super(props);
 
         this.state = {
-            open: false,
+            open: false, //Has the get journey time button been pressed for the card
             walkingDistance: null,
             transitDistance: null,
             bikingDistance: null,
-            apiCalled: false,
-            loading: true,
+            apiCalled: false, //Flag to see if api has already been called for the card
+            loading: true, 
 
 
         }
         this.showTravelTimes = this.showTravelTimes.bind(this);
     }
+
     showTravelTimes = () => {
         console.log("Works");
         this.setState({open: true});
@@ -31,12 +36,13 @@ class GetDirections extends React.Component {
         
     
         
-        
+        //Button must be clicked and the card must be expanded to make the request
         if (this.state.open && this.props.expanded){
+            //Creates a directionsService object that will be used to make the requests.
             const directionsService = new window.google.maps.DirectionsService();
             if (this.props.coords != null) {
                 if(this.state.loading){
-                                //Create request header using navigator position for google maps object
+                    //Create request header using navigator position for google maps object
                     const requestWalk = {
                         origin: this.props.coords.latitude + ", " + this.props.coords.longitude,
                         destination: this.props.questData['address'],
@@ -65,7 +71,7 @@ class GetDirections extends React.Component {
                                 this.setState({
                                     walkingDistance: result.routes[0].legs[0].duration.text,
                                 });
-                                console.log(result);
+                                
                             }
                         });
 
@@ -74,7 +80,7 @@ class GetDirections extends React.Component {
                                 this.setState({
                                     bikingDistance: result.routes[0].legs[0].duration.text,
                                 });
-                                console.log(result);
+                               
                             } else {
                                 console.log(status);
                             }
@@ -88,11 +94,12 @@ class GetDirections extends React.Component {
                                 });
                             }
                         });
-                        console.log(this.props.questData);
+                      
                         this.setState({apiCalled: true});
                     }
                 }
             }
+            //If all of the reponses from google have been received the repsonses are displayed
             if(!this.state.loading){
                 return(
                     <div>
@@ -106,15 +113,17 @@ class GetDirections extends React.Component {
                 )
             }
         }
+        //If the get travel times button has not been clicked the button is displayed
         if(!this.state.open){        
             return (
                 <div>
                     <Button size="small" color="primary" onClick={this.showTravelTimes}>
-                    Get Directions
+                        Get Travel Times
                     </Button>
                 </div>
             )
         }
+        //If The bget travel times button has been clicked but not all resonses have been received a loading bar is displayed.
         return(<LinearProgress color="primary" variant="query" />)
     }
 }
